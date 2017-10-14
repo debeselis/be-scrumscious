1e099f459219a7e64fe486ff90215c4f2a895cc3
shalikas <g.alasevicius@gmail.com>
2017-10-14 17:01:54 +0300

    start

---
+++ src/main/kotlin/org/scrumscious/bescrumscious/HomeController.kt
@@ -0,0 +1,14 @@
+package org.scrumscious.bescrumscious
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @RequestMapping ("/")
+    fun start (): String {
+        return "Hello Wtw"
+    }
+
+}
