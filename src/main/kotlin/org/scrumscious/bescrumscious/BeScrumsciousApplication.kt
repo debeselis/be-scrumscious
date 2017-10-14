96838cfd6aeb73fa8fa06cff25108dfce2795c5f
shalikas <g.alasevicius@gmail.com>
2017-10-14 16:00:11 +0300

    second

---
+++ src/main/kotlin/org/scrumscious/bescrumscious/BeScrumsciousApplication.kt
@@ -0,0 +1,11 @@
+package org.scrumscious.bescrumscious
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BeScrumsciousApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BeScrumsciousApplication::class.java, *args)
+}
---
+++ src/test/kotlin/org/scrumscious/bescrumscious/BeScrumsciousApplicationTests.kt
@@ -0,0 +1,16 @@
+package org.scrumscious.bescrumscious
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BeScrumsciousApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
